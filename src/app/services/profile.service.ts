669a44b5c44d8730e637e1bff89c6b34f9bac219
ZashyAris <sharonatieno080@gmail.com>
2020-01-21 05:47:03 +0300

    added  imports, created functionalities in the profile.service.ts

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpHeaders } from  '@angular/common/http';
-import 'rxjs/add/operator/map'; 
+import { HttpClient } from  '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +10,13 @@ export class ProfileService {
   private clientid = '06abd7f56f0d18419720';
   private clientsecret = 'f08d720eca830ae0480dbbb1cfae27f8f425ce41';
 
-  constructor(private http:HttpHeaders) { 
+  constructor(private http:HttpClient) { 
     console.log( "the service is working!");
     this.username = 'ZashyAris';
 
   }
   getProfileInfo(){
-    return this.http.get("https://api.github.com/users/" + this.username + "?client-id=" + this.clientid + "&client_secret=" this.clientsecret)
-    .localeCompare(res => res.json());
+    return this.http.get("https://api.github.com/users/" + this.username + "?client-id=" + this.clientid + "&clientsecret=" + this.clientsecret)
+  
   }
 }
